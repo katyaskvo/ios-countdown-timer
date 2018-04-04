162201babefaff58eab382d3bf5584e8f98bc12b
Ekaterina Zdorov <katyaskvo@gmail.com>
2018-04-04 10:44:16 +0300

    add 2 mins button and corresponding timer

--- timer/TimerViewController.swift
+++ timer/TimerViewController.swift
@@ -13,18 +13,30 @@ class TimerViewController: UIViewController {
   @IBAction func munute1tapped(_ sender: UIButton) {
         performSegue(withIdentifier: "timer1min", sender: self)
     }
-   override func viewDidLoad() {
+    @IBAction func minutes2tapped(_ sender: Any) {
+        performSegue(withIdentifier: "timer2mins", sender: self)
+    }
+    override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // 1 min
         if (segue.identifier == "timer1min") {
             let destVC : CountDownViewController = segue.destination as! CountDownViewController
             destVC.numberOfSeconds = 60
             destVC.runTimer()
-
         }
+        
+        // 2 mins
+        if (segue.identifier == "timer2mins") {
+            let destVC : CountDownViewController = segue.destination as! CountDownViewController
+            destVC.numberOfSeconds = 120
+            destVC.runTimer()
+        }
+
     }
 }
 
