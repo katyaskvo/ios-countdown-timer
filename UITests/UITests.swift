47122e32538243d8c4e8b5be9349cb7d3fa82aec
Katya Zdorov and Paul Zabelin <katyaskvo+katyaskvo+p.zabelin@gmail.com>
2018-09-21 16:07:00 -0700

    add 1 minute timer test

--- UITests/UITests.swift
+++ UITests/UITests.swift
@@ -1,34 +1,23 @@
-//
-//  UITests.swift
-//  UITests
-//
-//  Created by Paul Zabelin on 9/21/18.
-//  Copyright © 2018 Ekaterina Zdorov. All rights reserved.
-//
-
 import XCTest
 
 class UITests: XCTestCase {
+    let app = XCUIApplication()
+    var oneMinuteTimer: XCUIElement!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launch()
+        oneMinuteTimer = app.buttons["1"]
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    func testOneMinuteTimerWithImmediateClose() {
+        oneMinuteTimer.tap()
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        let exists = NSPredicate(format: "exists != 1")
+        expectation(for: exists, evaluatedWith: oneMinuteTimer, handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
 
+        app.buttons["close button"].tap()
+        XCTAssert(oneMinuteTimer.exists, "close should return to the list of timers")
+    }
 }
